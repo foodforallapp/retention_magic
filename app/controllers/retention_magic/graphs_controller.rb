5b6e78b53e91288e5c7173dcef1a6d9e083a468a
Victor Carreno <viccarre@gmail.com>
2019-05-07 16:37:51 -0400

    change period

--- app/controllers/retention_magic/graphs_controller.rb
+++ app/controllers/retention_magic/graphs_controller.rb
@@ -7,14 +7,14 @@ module RetentionMagic
       @user_class = RetentionMagic.user_class.constantize
 
       @first_user = @user_class.order(:created_at).first
-      first_cohort_week = 12.weeks.ago.to_date.beginning_of_week
+      first_cohort_month = 12.months.ago.to_date.beginning_of_month
       today = Date.today
 
-      difference_in_days_between_now_and_first_cohort = (today.beginning_of_week - first_cohort_week).to_i
+      difference_in_days_between_now_and_first_cohort = (today.beginning_of_week - first_cohort_month).to_i
 
       # For presenting the widgets
       @current_cohort_label = today.strftime("%U/%Y")
-      @number_of_weeks = difference_in_days_between_now_and_first_cohort / 7
+      @number_of_months = difference_in_days_between_now_and_first_cohort / 30
 
       @graphs = {
         activation: {
@@ -28,9 +28,9 @@ module RetentionMagic
       retention_models = RetentionMagic.retention_models.map { |class_name| class_name.constantize }
       activation_fields_for_query = RetentionMagic.activation_counter_columns.join(" + ")
 
-      0.upto(@number_of_weeks) do |week|
-        cohort_start = first_cohort_week + week.weeks
-        cohort_start_end = (cohort_start + 1.week).beginning_of_week
+      0.upto(@number_of_months) do |month|
+        cohort_start = first_cohort_month + month.months
+        cohort_start_end = (cohort_start + 1.month).beginning_of_month
 
         cohort_users = @user_class.where(created_at: cohort_start..cohort_start_end)
         number_of_cohort_users = cohort_users.count
@@ -61,20 +61,20 @@ module RetentionMagic
         cohort_user_ids = cohort_users.pluck(:id)
         cohort_size = cohort_users.count
 
-        difference_in_days_between_now_and_cohort_start = (today.beginning_of_week - cohort_start.to_date).to_i
-        number_of_weeks_in_this_cohort = difference_in_days_between_now_and_cohort_start / 7
+        difference_in_days_between_now_and_cohort_start = (today.beginning_of_month - cohort_start.to_date).to_i
+        number_of_months_in_this_cohort = difference_in_days_between_now_and_cohort_start / 7
 
-        0.upto(number_of_weeks_in_this_cohort) do |week|
-          retention_week = (cohort_start + week.weeks).beginning_of_week
-          if week == 0
-            @graphs[:retention][cohort_start_label][retention_week] = cohort_size
+        0.upto(number_of_months_in_this_cohort) do |month|
+          retention_month = (cohort_start + month.months).beginning_of_month
+          if month == 0
+            @graphs[:retention][cohort_start_label][retention_month] = cohort_size
           else
-            retention_week_end = (retention_week + 1.week).beginning_of_week
+            retention_month_end = (retention_month + 1.month).beginning_of_month
 
             records_per_user = {}
 
             retention_models.each do |model|
-              count_per_user = model.where(user_id: cohort_user_ids).where(created_at: retention_week..retention_week_end).group("user_id").count
+              count_per_user = model.where(user_id: cohort_user_ids).where(created_at: retention_month..retention_month_end).group("user_id").count
               count_per_user.each do |uid, count|
                 records_per_user[uid] ||= 0
                 records_per_user[uid] += count
@@ -82,9 +82,9 @@ module RetentionMagic
             end
 
             if cohort_user_ids.empty?
-              @graphs[:retention][cohort_start_label][retention_week] = 0
+              @graphs[:retention][cohort_start_label][retention_month] = 0
             else
-              @graphs[:retention][cohort_start_label][retention_week] = records_per_user.keys.size
+              @graphs[:retention][cohort_start_label][retention_month] = records_per_user.keys.size
             end
           end
         end
